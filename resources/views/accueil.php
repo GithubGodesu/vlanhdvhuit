de8da476b8db93b5c07ca9631deb48274c575cb1
NEOXHEAD <145574300+NEOXHEAD@users.noreply.github.com>
2024-02-06 16:24:29 +0100

    modif boutons

---
+++ resources/views/accueil.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemple de mise en page CSS</title>
+    <link rel="stylesheet" href="stylesHDVHUIT.css">
+</head>
+<body>
+    <header>
+        <h1>Accueil</h1>
+        <nav>
+            <ul>
+
+            </ul>
+        </nav>
+    </header>
+    
+    <section class="content">
+        <article>
+            <h2></h2>
+            <p></p>
+        </article>
+    </section>
+
+    <footer>
+        <p>&copy; 2024 Mon Site Web. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
